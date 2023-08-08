a782bd72e46204efd661e3d32a793140fbbde4ee
Navodayan Amit <98446912+a21amit@users.noreply.github.com>
2023-08-08 20:30:26 +0530

    Add files via upload

---
+++ sketch_aug8a_buz_pot.ino
@@ -0,0 +1,20 @@
+int buzzPin=8;
+int potPin=A3;
+int potVal;
+int dt=500;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(buzzPin,OUTPUT);
+pinMode(potPin,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  potVal=analogRead(potPin);
+  Serial.println(potVal);
+  if(potVal>1000){
+    digitalWrite(buzzPin,HIGH);
+    delay(dt);
+    digitalWrite(buzzPin,LOW);
+  }
+}
