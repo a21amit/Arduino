3781ba2b09c5273c3dca2ecc5c4f844c2ed59583
Navodayan Amit <98446912+a21amit@users.noreply.github.com>
2023-08-08 19:46:05 +0530

    Add files via upload

---
+++ sketch_aug8a_activebuzzer/sketch_aug8a_activebuzzer.ino
@@ -0,0 +1,24 @@
+int buzzPin=13;
+int myNUm;
+String msg="input your number: ";
+int dt=500;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(buzzPin,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println(msg);
+while(Serial.avialable()==0){
+
+}
+myNum=Serial.parseInt();
+if(myNum > 10){
+  digitalWrite(buzzPin,High);
+  delay(dt);
+  digitalWrite(buzzPin,LOW);
+  delay(dt);
+}
+}
